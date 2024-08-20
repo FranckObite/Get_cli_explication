f16420908f7af7cc9d4add9b8e1956268ebc5223
FranckObite <obitefrank@gmail.com>
2024-08-20 20:47:13 +0000

    first commit

---
+++ android/app/src/main/kotlin/com/getclitest/get_cli_test/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.getclitest.get_cli_test
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/generated/locales.g.dart
@@ -0,0 +1,39 @@
+// DO NOT EDIT. This is code generated via package:get_cli/get_cli.dart
+
+// ignore_for_file: lines_longer_than_80_chars
+// ignore: avoid_classes_with_only_static_members
+class AppTranslation {
+  static Map<String, Map<String, String>> translations = {
+    'en_EN': Locales.en_EN,
+    'pt_BR': Locales.pt_BR,
+  };
+}
+
+class LocaleKeys {
+  LocaleKeys._();
+  static const butons_login = 'butons_login';
+  static const butons_sign_in = 'butons_sign_in';
+  static const butons_logout = 'butons_logout';
+  static const butons_Sign_in_fb = 'butons_Sign_in_fb';
+  static const butons_sing_in_google = 'butons_sing_in_google';
+  static const butons_sign_in_apple = 'butons_sign_in_apple';
+}
+
+class Locales {
+  static const en_EN = {
+    'butons_login': 'Login',
+    'butons_sign_in': 'Sign-in',
+    'butons_logout': 'Logout',
+    'butons_Sign_in_fb': 'Sign-in with Facebook',
+    'butons_sing_in_google': 'Sign-in Google',
+    'butons_sign_in_apple': 'Sign-in Apple',
+  };
+  static const pt_BR = {
+    'butons_login': 'Entrar',
+    'butons_sign_in': 'Cadastrar-se',
+    'butons_logout': 'Sair',
+    'butons_Sign_in_fb': 'Entrar com o Facebook',
+    'butons_sing_in_google': 'Entrar com o Google',
+    'butons_sign_in_apple': 'Entra com o Apple',
+  };
+}
